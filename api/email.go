4ef7726e6ea2ae3425f03a55b8475d6e972f8a88
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2025-02-23 13:43:42 +0900

    add Mail func

---
+++ api/email.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+func SendEmail(config Config, from, body, subject string, to []string) error {
+	auth := smtp.PlainAuth("", config.SmtpUser, config.SmtpPassword, config.SmtpHost)
+
+	message := "Subject: " + subject + "\n" + "Content-Type: text/html; charset=utf-8" + body
+
+	err := smtp.SendMail(config.SmtpHost+":"+config.SmtpPort, auth, from, to, []byte(message))
+	if err != nil {
+		return fmt.Errorf("send email failed")
+	}
+
+	return nil
+}
---
+++ util/config.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	EmailPort           string        `mapstructure:"EMAIL_PORT"`
+	Environment         string        `mapstructure:"ENVIRONMENT"`
+	AllowHeaders        []string      `mapstructure:"ALLOW_HEADERS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	SmtpUser            string        `mapstructure:"SMTP_USER"`
+	SmtpPassword        string        `mapstructure:"SMTP_PASSWORD"`
+	SmtpHost            string        `mapstructure:"SMTP_HOST"`
+	SmtpPort            string        `mapstructure:"SMTP_PORT"`
+}
+
+func LoadConfig(path string) (Config, error) {
+	var config Config
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		return Config{}, err
+	}
+
+	err = viper.Unmarshal(&config)
+
+	return config, nil
+}
